f47ea562ea3142aa155ad9b5cf71e765a58d317d
Karen Go <karenrgo@gmail.com>
2019-06-06 13:41:01 -0700

    arm wrestling algo challenge

---
+++ arm-wrestling/arm_wrestling.rb
@@ -0,0 +1,34 @@
+# //Given the lifting strength of the arms of two people.
+# //Determine the results of a arm wrestling match between the two
+# //[leftArm,rightArm]
+require 'pry'
+playerOne = [10,13]
+playerTwo = [11,9]
+
+def wrestle(playerOne_a, playerTwo_a)
+	for i in 0..1
+		arms = ["Left Arm", "Right Arm"]
+		case playerOne_a[i] <=> playerTwo_a[i]
+		when -1
+			puts "#{arms[i]} winner, playerTwo"
+		when 0
+			puts "#{arms[i]} tie"
+		when 1
+			puts "#{arms[i]} winner, playerOne"
+		end
+	end
+end
+
+wrestle(playerOne, playerTwo)
+binding.pry
+
+wrestle([11,10],[11,9])
+binding.pry
+
+# //in: playerOne,PlayerTwo
+# //out: Left Arm winner, playerTwo
+# //     Right Arm winner, playerOne
+
+# //in: [11,10], [11,9]
+# //out: Left arm tie 
+# //     Right arm winner, playerOne
