fd007842f1449a9bffa391b9bbd9e29ccbb1e6ee
Karen Go <karenrgo@gmail.com>
2019-06-14 16:40:28 -0700

    2nd solution

--- flatten-multidim-array/flatten-ND-array.rb
+++ flatten-multidim-array/flatten-ND-array.rb
@@ -1,6 +1,24 @@
 require 'pry'
 a = [2,[3,4],[3,[4,[5,[]]]]]
-output = a.join.split(//)
+# output = a.join.split(//)
+
+output = []
+while !a.empty?
+    x = a.shift
+    if x.class != Array
+        output << x
+        a
+    else
+        for i in 0..x.length-1
+            a.insert(i,x[i])
+        end
+        a
+    end
+end
+output
+
+
+
 
 binding.pry
 0
